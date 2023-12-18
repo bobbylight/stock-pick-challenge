0bbd309d73aedf37abb09d0852ee10088c86b690
bobbylight <robert.e.futrell@gmail.com>
2023-12-18 11:30:44 -0500

    Bumping vue-cli dependency

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -7,12 +7,15 @@ module.exports = {
     'plugin:vue/essential',
     'eslint:recommended'
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/valid-v-slot': [ 'error', { allowModifiers: true, }, ],
+    'vue/multi-word-component-names': 'off', // TODO: Change about.vue and user.vue to multi-word
   },
   overrides: [
     {
