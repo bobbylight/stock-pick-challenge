a9776898b9d71b8f2455c48eb36bdd0048363f8d
bobbylight <robert.e.futrell@gmail.com>
2024-12-29 17:35:54 -0500

    Fixing lint issue

--- app/src/nav-banner.vue
+++ app/src/nav-banner.vue
@@ -11,7 +11,7 @@
         <v-menu>
           <template #activator="{ props }">
             <a
-                class="year-selector"
+              class="year-selector"
               v-bind="props"
             >{{ store.year }}</a>
           </template>
